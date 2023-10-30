c151dcf88275dda23fb0a378688b04df295f9057
Cherem2002 <118422320+Cherem2002@users.noreply.github.com>
2023-10-30 11:41:12 +0800

    Add files via upload

---
+++ client/src/Pages/Analytics/Analytics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Analytics.css';
+
+function Analytics() {
+    return (
+        <div className='Home'>
+            Аналитика
+        </div>
+    );
+}
+
+export default Analytics;
