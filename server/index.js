261aec527d53b777b8bf86a387925adaea5d7b9f
Cherem2002 <118422320+Cherem2002@users.noreply.github.com>
2024-03-19 15:46:59 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -26,32 +26,44 @@ app.post('/presence', (req, res, next) => {
 
   // Преобразуйте "UID" из данных ESP32 в строку
   const idCardFromESP32 = data.UID;
+  const idSensor = 1;
+  const currentTime = new Date();
 
   // Получите данные из базы данных и верните Id_Student в случае совпадения
-  pool.query('SELECT id_student FROM student WHERE uid_student = $1', [idCardFromESP32])
+  pool.query('SELECT id_student FROM student,sensor WHERE uid_student = $1 and bid_sensor = $2', [idCardFromESP32,idSensor])
     .then(result => {
       const matchingStudent = result.rows[0];
-
       if (matchingStudent) {
-        console.log('Студент найден:', matchingStudent);
-        // Выполните обновление Presence на 1
-        pool.query('UPDATE attendance SET presence = 1 WHERE id_student = $1', [matchingStudent.id_student])
-          .then(() => {
-            console.log('Значение Presence обновлено на 1');
-            res.status(200).send('Attended');
-          })
-          .catch(error => {
-            console.error('Ошибка при обновлении Presence:', error);
-            res.status(500).send('Ошибка сервера');
-          });
+        console.log('Студент найден:', matchingStudent);  
+        pool.query(
+        `SELECT s.id_group 
+        FROM student AS st 
+        JOIN schedule AS s ON st.id_group = s.id_group 
+        WHERE st.id_student = $1
+        AND to_timestamp(s.time, 'HH24:MI') <= current_timestamp 
+        AND to_timestamp(s.time, 'HH24:MI') > current_timestamp - interval '1 minute'`,
+        [matchingStudent.id_student]
+        )
+        .then(result => {
+          // Обработка результата запроса здесь
+          if (result.rows.length > 0) {
+            // Студент найден и пришел вовремя или с опозданием до 1 минуты
+            console.log('Студент найден и пришел вовремя или с опозданием до 1 минуты');
+            // Ваш код для этого случая
+        } else {
+            // Студент найден и опоздал более, чем на 1 минуту
+            console.log('Студент найден и опоздал более, чем на 1 минуту');
+            // Ваш код для этого случая
+        }
+        })     
       } else {
         console.log('Студент не найден');
-        res.status(404).send('Not found');
+        res.status(200).send('Not found');
       }
     })
     .catch(error => {
       console.error('Произошла ошибка:', error);
-      res.status(500).send('Ошибка сервера');
+      res.status(500).send('Server mistake');
     });
 })
 
@@ -125,53 +137,108 @@ app.get('/discipline/:idTeacher', async (req, res) => {
 });
 
 app.get('/chart-data', async (req, res) => {
-  const { selectedGroups, selectedSubjects, startDate, endDate,idTeacher } = req.query;
+  const { selectedGroups, selectedSubjects, startDate, endDate, idTeacher, Type } = req.query;
 
   try {
-    const groupColors = generateUniqueColors(selectedGroups.length);
-    const result = await pool.query(
-      `SELECT 
-        schedule.id_schedule, 
-        groups.name_group, 
-        discipline.name_discipline, 
-        COUNT(attendance.id_attendance) AS attendance
-      FROM 
-        schedule
-        LEFT JOIN groups ON schedule.id_group = groups.id_group
-        LEFT JOIN discipline ON schedule.id_discipline = discipline.id_discipline
-        LEFT JOIN attendance ON schedule.id_schedule = attendance.id_schedule
-      WHERE 
-        schedule.id_teacher = $1
-        AND groups.name_group = ANY($2::text[])
-        AND discipline.name_discipline = ANY($3::text[])
-        AND schedule.date BETWEEN $4 AND $5
-      GROUP BY 
-        schedule.id_schedule, 
-        groups.name_group, 
-        discipline.name_discipline`,
-      [idTeacher, selectedGroups, selectedSubjects, startDate, endDate]
-    );
+    if (Type == 2) {
+      console.log('По группе');
+      console.log(idTeacher);
+      console.log(selectedGroups);
+      console.log(selectedSubjects);
+      console.log(startDate);
+      console.log(endDate);
+      // Построение бар-диаграммы посещаемости одной группы по многим предметам
+      const groupColors = generateUniqueColors(selectedGroups.length); // Один цвет для одной группы
+      const result = await pool.query(
+        `SELECT 
+          discipline.name_discipline, 
+          COUNT(CASE WHEN attendance.presence = 1 THEN 1 ELSE NULL END) AS attended,
+          COUNT(*) AS total,
+          (COUNT(CASE WHEN attendance.presence = 1 THEN 1 ELSE NULL END)::float / COUNT(*)) * 100 AS attendance_percentage
+        FROM 
+          schedule
+          LEFT JOIN discipline ON schedule.id_discipline = discipline.id_discipline
+          LEFT JOIN attendance ON schedule.id_schedule = attendance.id_schedule
+        WHERE 
+          schedule.id_teacher = $1
+          AND schedule.id_group = (SELECT id_group FROM groups WHERE name_group = $2)
+          AND discipline.name_discipline = ANY($3::text[])
+          AND schedule.date BETWEEN $4 AND $5
+        GROUP BY 
+          discipline.name_discipline`,
+        [idTeacher, selectedGroups[0], selectedSubjects, startDate, endDate]
+      );
+      console.log('Labels:', selectedSubjects.map(subject => subject.label));
+      console.log('Data:', result.rows.map(item => parseInt(item.attendance_percentage)));
+      const barData = {
+        labels: selectedSubjects,
+        datasets: [
+          {
+            label: 'Посещаемость',
+            data: result.rows.map(item => parseInt(item.attendance_percentage)),
+            backgroundColor: groupColors,
+            borderColor: groupColors,
+            borderWidth: 2,
+          },
+        ],
+      };
 
-    const radarData = {
-      labels: selectedSubjects,
-      datasets: result.rows.map(item => {
-        const color = groupColors[selectedGroups.indexOf(item.name_group)];
-        return {
-          label: item.name_group,
-          data: [item.attendance],
-          backgroundColor: color,
-          borderColor: color,
-          borderWidth: 2,
-        };
-      }),
-    };
+      res.json(barData);
+      console.log('Result from the database:', result.rows);
+    } else if (Type == 1) {
+      console.log('По предмету');
+      console.log(idTeacher);
+      console.log(selectedGroups);
+      console.log(selectedSubjects);
+      console.log(startDate);
+      console.log(endDate);
+      // Построение бар-диаграммы посещаемости многих групп по одному предмету
+      const groupColors = generateUniqueColors(selectedSubjects.length);
+      const result = await pool.query(
+        `SELECT 
+          groups.name_group, 
+          COUNT(CASE WHEN attendance.presence = 1 THEN 1 ELSE NULL END) AS attended,
+          COUNT(*) AS total,
+          (COUNT(CASE WHEN attendance.presence = 1 THEN 1 ELSE NULL END)::float / COUNT(*)) * 100 AS attendance_percentage
+        FROM 
+          schedule
+          LEFT JOIN groups ON schedule.id_group = groups.id_group
+          LEFT JOIN attendance ON schedule.id_schedule = attendance.id_schedule
+        WHERE 
+          schedule.id_teacher = $1
+          AND groups.name_group = ANY($2::text[])
+          AND schedule.id_discipline = (SELECT id_discipline FROM discipline WHERE name_discipline = $3)
+          AND schedule.date::date BETWEEN $4 AND $5
+        GROUP BY 
+          groups.name_group`,
+        [idTeacher, selectedGroups, selectedSubjects[0], startDate, endDate]
+      );
 
-    res.json(radarData);
+      const barData = {
+        labels: selectedGroups,
+        datasets: [
+          {
+            label: 'Посещаемость',
+            data: result.rows.map(item => parseInt(item.attendance_percentage)),
+            backgroundColor: groupColors,
+            borderColor: groupColors,
+            borderWidth: 2,
+          },
+        ],
+      };
+
+      res.json(barData);
+      console.log('Result from the database:', result.rows);
+
+    } else {
+      res.status(400).json({ error: 'Invalid parameters' });
+    }
   } catch (error) {
     console.error('Ошибка при выполнении запроса:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
 
 function generateUniqueColors(count) {
   const colors = [];
